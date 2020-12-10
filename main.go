154fbc85bbbdce8a161615dfaffb805754966c14
Ted Lilley <ted.lilley@digi.com>
2020-12-09 19:08:17 -0500

    s3

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-digitalocean/sdk/v3/go/digitalocean"
+	do "github.com/pulumi/pulumi-digitalocean/sdk/v3/go/digitalocean"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create a DigitalOcean resource (Domain)
-		domain, err := digitalocean.NewDomain(ctx, "my-domain", &digitalocean.DomainArgs{
-			Name: pulumi.String("my-domain.io"),
+		// Create a DigitalOcean resource (SpacesBucket)
+		bucket, err := do.NewSpacesBucket(ctx, "hello-world", &do.SpacesBucketArgs{
+			Acl:    pulumi.String("public-read"),
+			Region: pulumi.String("nyc3"),
 		})
 		if err != nil {
 			return err
 		}
 
-		// Export the name of the domain
-		ctx.Export("domainName", domain.Name)
+		_, err = do.NewSpacesBucketObject(ctx, "index.html", &do.SpacesBucketObjectArgs{
+			Bucket:      bucket.Name,
+			Key:         pulumi.String("index.html"),
+			Acl:         pulumi.String("public-read"),
+			Content:     pulumi.String("<html>Hello, World!</html>"),
+			ContentType: pulumi.String("text/html"),
+			Region:      pulumi.String("nyc3"),
+		},
+			pulumi.DependsOn([]pulumi.Resource{bucket}),
+		)
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("bucketDomainName", bucket.BucketDomainName)
+
 		return nil
 	})
 }
